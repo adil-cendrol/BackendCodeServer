4e560063870d71acfae85551ac70cc8e137f81e6
Adil nawaz <adilnawaz256@gmail.com>
2025-10-08 14:38:44 +0530

    add this

--- server.mjs
+++ server.mjs
@@ -223,5 +223,6 @@ function stopRecording() {
 }
 
 
+// const PORT = process.env.PORT || 8080;
 const PORT = process.env.PORT || 8080;
-server.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
+server.listen(PORT, "0.0.0.0", () => console.log(`✅ Server running on http://0.0.0.0:${PORT}`));
